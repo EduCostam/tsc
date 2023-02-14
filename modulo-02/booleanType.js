bc09a7fa3967fc9ffa9d6771732877c7bac85f09
EduCostam <eduardomc.devs@gmail.com>
2023-02-14 18:17:54 -0300

    booleanTypes

---
+++ modulo-02/booleanType.js
@@ -0,0 +1,14 @@
+"use strict";
+// => Exemplo 01
+let tarefaConcluida = true;
+let tarefaOebdebte = false;
+console.log(tarefaConcluida);
+console.log(tarefaOebdebte);
+// => Exemplo 02
+let concluido = false;
+if (!concluido) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa Pendente!');
+}
---
+++ modulo-02/booleanType.ts
@@ -0,0 +1,16 @@
+// => Exemplo 01
+let tarefaConcluida: boolean = true;
+let tarefaOebdebte: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaOebdebte);
+
+// => Exemplo 02
+
+let concluido: boolean = false;
+
+if (!concluido) {
+    console.log('Tarefa foi concluída com sucesso!')
+} else {
+    console.log('Tarefa Pendente!');
+}
