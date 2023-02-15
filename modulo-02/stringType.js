08f5ed9062ff25dec03c311cf366018d88c35b7d
EduCostam <eduardomc.devs@gmail.com>
2023-02-15 16:28:34 -0300

    stringType

---
+++ modulo-02/stringType.js
@@ -0,0 +1,12 @@
+"use strict";
+// => Exemplo 01 - Single Quotes
+let nomeCompleto = 'Eduardo Costa';
+console.log(nomeCompleto);
+// => Exemplo 02 - Double Quotes
+let funcaooEmpresa = "Cloud Advocate em JavaScript";
+console.log(funcaooEmpresa);
+// => Exemplo 03 - Back Ticks
+let nomeEmpresa = 'Microsoft';
+let dadosFuncionario = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaooEmpresa}
+na empresa ${nomeEmpresa}`;
+console.log(dadosFuncionario);
---
+++ modulo-02/stringType.ts
@@ -0,0 +1,18 @@
+// => Exemplo 01 - Single Quotes
+
+let nomeCompleto: string = 'Eduardo Costa';
+console.log(nomeCompleto);
+
+// => Exemplo 02 - Double Quotes
+
+let funcaooEmpresa: string = "Cloud Advocate em JavaScript";
+console.log(funcaooEmpresa);
+
+// => Exemplo 03 - Back Ticks
+
+let  nomeEmpresa: string = 'Microsoft';
+
+let dadosFuncionario:  string = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaooEmpresa}
+na empresa ${nomeEmpresa}`;
+
+console.log(dadosFuncionario);
