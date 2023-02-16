0e643349383fb46c500e8f3cbbc3e510010e5034
EduCostam <eduardomc.devs@gmail.com>
2023-02-16 15:54:42 -0300

    package-lock.json

---
+++ modulo-02/enumType.ts
@@ -0,0 +1,16 @@
+// => Exemplo 01: Numeric Enums (Enums Numéricos)
+
+enum Idioma {
+  Portugues,
+  Espanhol,
+  Ingles,
+  Frances,
+}
+
+console.log(Idioma);
+
+// => Exemplo 02: String Enums
+
+// => Exemplo 03: Como podemos acessar um valor de um Enum com uma chave: (usando o const)
+
+// => Exemplo 04: Quando usar enum?
