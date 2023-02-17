1f297a825347c0b2151c5346b331c5f8e5aaae97
EduCostam <eduardomc.devs@gmail.com>
2023-02-17 14:42:36 -0300

    voidType.ts

---
+++ modulo-02/voidType.ts
@@ -0,0 +1,23 @@
+// => Exemplo 01 - funções:
+function logError(errorMessage: string): void {
+  console.log(errorMessage);
+  //return errorMessage;
+}
+
+logError('Required field - Name!');
+
+// => Exemplo 02 - funções:
+const logErrorExample2 = (errorMessage: string) => {
+  console.log(errorMessage);
+};
+
+logErrorExample2('Required field - Surname');
+
+// => Exemplo void: variáveis
+
+let variavelExemploVoid: void;
+// variavelExemploVoid = 1;
+variavelExemploVoid = null;
+variavelExemploVoid = undefined;
+
+console.log(variavelExemploVoid);
