2395b89d7784bc1012bf37db4280f40b9493ffdd
EduCostam <eduardomc.devs@gmail.com>
2023-02-17 15:25:43 -0300

    nullUndefinedType.ts

---
+++ modulo-02/nullUndefinedType.ts
@@ -0,0 +1,17 @@
+// => Exemplo 01:
+
+let variavelTesteNull = null;
+
+console.log(variavelTesteNull);
+console.log(typeof variavelTesteNull);
+
+// => Exenmplo 02: Undefined
+
+let variavelTesteUndefined;
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+// => Diferenças e Similaridades: Null vs Undefined
+
+console.log('Exemplo01:', null == undefined);
+console.log('Exemplo02:', null === undefined);
