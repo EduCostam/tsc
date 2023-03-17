9defa08e3a9adffd109df1c5bd20a442eac1766a
EduCostam <eduardomc.devs@gmail.com>
2023-03-17 11:30:56 -0300

    interface.ts

---
+++ modulo-06/interface.ts
@@ -0,0 +1,11 @@
+export {};
+
+// => Exemplo 01: Interface Simples
+
+// => Exemplo 02: Interface com Propriedades Opcionais
+
+// => Exemplo 03: Interface com Propriedades de Somente Leitura e Opcionais
+
+// => Exemplo 04: Interface com implements Class
+
+// => Exemplo 05: Interface vs Type Alias
