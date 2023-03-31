1eba1a6370a1fc974006990969403b4900a9c155
EduCostam <eduardomc.devs@gmail.com>
2023-03-30 21:16:34 -0300

    interface.ts

--- modulo-06/interface.ts
+++ modulo-06/interface.ts
@@ -2,7 +2,42 @@ export {};
 
 // => Exemplo 01: Interface Simples
 
+interface Pessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+}
+
+function exibirNome(pessoa: Pessoa) {
+  return `
+    Nome...: ${pessoa.nome}
+    Sobrenome...: ${pessoa.sobrenome}
+    Idade...: ${pessoa.idade}
+  `;
+}
+
+const eduardo = {
+  nome: 'Eduardo',
+  sobrenome: 'Costa',
+  idade: 25,
+};
+
+console.log(exibirNome(eduardo));
+
 // => Exemplo 02: Interface com Propriedades Opcionais
+
+interface Livro {
+  titulo: string;
+  autor: string;
+  paginas?: number; // Propriedade opcional
+}
+
+const livro: Livro = {
+  titulo: 'Clean Code',
+  autor: 'Robert C. Martin',
+};
+
+console.log(livro);
 
 // => Exemplo 03: Interface com Propriedades de Somente Leitura e Opcionais
 
