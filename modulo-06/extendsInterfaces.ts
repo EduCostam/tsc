41eaa959b4f80cde8e41eb638de5af8201dd0a36
EduCostam <eduardomc.devs@gmail.com>
2023-04-11 18:38:03 -0300

    extendsInterface.ts

--- modulo-06/extendsInterfaces.ts
+++ modulo-06/extendsInterfaces.ts
@@ -7,6 +7,19 @@ interface Animal {
   porte: string;
 }
 
+interface Cachorro extends Animal {
+  raca: string;
+}
+
+const cachorro: Cachorro = {
+  nome: 'Alface',
+  idade: 7,
+  porte: 'Pequeno-médio',
+  raca: 'SRD',
+};
+
+console.log(cachorro);
+
 // => Exemplo 02 - Extensão de Múltiplas Interfaces
 
 // => Exemplo 03 - Uso do Omit
