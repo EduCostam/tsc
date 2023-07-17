ca7cd26872adf2cb8e7ae15c3cd149ebcbca845e
EduCostam <eduardomc.devs@gmail.com>
2023-07-16 21:28:33 -0300

    tupeAnnotation.ts

--- modulo-02/typeAnnotation.ts
+++ modulo-02/typeAnnotation.ts
@@ -7,17 +7,17 @@ let animais: string[] = ['Elefante', 'Cachorro', 'Panda', 'Girafa'];
 console.log(animais);
 
 // => Objetos [Type Annotations]
-let carro: {
-    nome: string;
-    ano: number;
-    preco: number
-};
+//let carro: {
+//nome: string;
+//ano: number;
+//preco: number
+//};
 
-carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 }
-console.log(carro);
+//carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 }
+//console.log(carro);
 
 // => Functions [Type Annotation]
 function multiplicarNumero(num1: number, num2: number) {
-    return num1 * num2;
+  return num1 * num2;
 }
-console.log(multiplicarNumero(2,5));
+console.log(multiplicarNumero(2, 5));
