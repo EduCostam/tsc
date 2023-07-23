eff297f28a52a05450eb61893335f6ce72450b14
EduCostam <eduardomc.devs@gmail.com>
2023-07-23 16:46:38 -0300

    typeassertion.ts

--- modulo-07/type assertion/typeassertion.ts
+++ modulo-07/type assertion/typeassertion.ts
@@ -10,3 +10,20 @@ function exibirPrecoFinal(preco: number, desconto: number, formato: boolean): nu
 
 const descontoFinal = exibirPrecoFinal(100, 0.05, true) as string;
 console.log(descontoFinal);
+
+// Exemplo 02 - Type Assertion
+
+/*type Humano = {
+  nome: string;
+  idade: number;
+  idioma: string;
+}
+
+const humano = {
+  nome: 'José',
+  idade: 25,
+}
+
+const humano_02: Humano = humano;
+console.log(humano_02.nome.toLocaleUpperCase());*/
+
